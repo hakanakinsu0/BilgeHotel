b0d1c951d70f1b79d78c6d209c42304a6c1fa6a9
hakanakinsu0 <hakanakinsu@outlook.com>
2025-02-21 01:33:48 +0300

    UI icin yeniden baslanildi

---
+++ Project.MvcUI/Models/PageVms/ReservationPageVm.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Project.MvcUI.Models.PureVms.Reservations.ResponseModels;
+
+namespace Project.MvcUI.Models.PureVms.Reservations.PageVms
+{
+    public class ReservationPageVm
+    {
+        public List<ReservationResponseModel> Reservations { get; set; }
+    }
+}
