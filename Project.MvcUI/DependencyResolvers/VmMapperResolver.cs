0c38508c4e870dec0a7b90e0716636f2d550ebb8
hakanakinsu0 <hakanakinsu@outlook.com>
2025-04-03 18:20:44 +0300

    MvcUI katmaninda DependencyResolvers klasoru icindeki class refaktor edildi.

--- Project.MvcUI/DependencyResolvers/VmMapperResolver.cs
+++ Project.MvcUI/DependencyResolvers/VmMapperResolver.cs
@@ -3,8 +3,15 @@ using Project.MvcUI.VmMapping;
 
 namespace Project.MvcUI.DependencyResolvers
 {
+    /// <summary>
+    /// ViewModel katmanındaki AutoMapper profilini DI konteynerine eklemek için kullanılan genişletme sınıfıdır.
+    /// </summary>
     public static class VmMapperResolver
     {
+        /// <summary>
+        /// AutoMapper konfigürasyonunu ViewModel Mapping profiliyle birlikte servislere ekler.
+        /// </summary>
+        /// <param name="services">IServiceCollection nesnesi</param>
         public static void AddVmMapperService(this IServiceCollection services)
         {
             services.AddAutoMapper(typeof(VmMappingProfile));
